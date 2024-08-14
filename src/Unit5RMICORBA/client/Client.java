82d4787db7d3a86c22db1d3d5aae9d211fc15b8a
ojan <ojan.adhikari@wealthdoor.com>
2024-08-14 17:39:24 +0545

    changes

--- src/Unit5RMICORBA/client/Client.java
+++ src/Unit5RMICORBA/client/Client.java
@@ -8,7 +8,7 @@ import java.rmi.registry.Registry;
 public class Client {
     public static void main(String args[]) {
         try {
-            Registry registry = LocateRegistry.getRegistry();
+            Registry registry = LocateRegistry.getRegistry("localhost",8080);
             Hello stub = (Hello) registry.lookup("Hello");
             String response = stub.sayHello();
             System.out.println("Response: " + response);
